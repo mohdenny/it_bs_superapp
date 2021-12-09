f2b529c03c01e08af5a43aeedd3e591953e9d63e
Mohammad Denny <mohammaddenny94@gmail.com>
2021-12-09 17:22:20 +0700

    import actions, and render props

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,30 +1,45 @@
 import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { getDiskSpace } from '../../actions/storage'
+import { getAllDisk, getDiskSpace } from '../../actions/disk'
 
-// const Dashboard = ({ getDiskSpace }) => {
-const Dashboard = ({ getDiskSpace, disk: { disk } }) => {
+const Dashboard = ({ getAllDisk, getDiskSpace, disk: { disk, disks } }) => {
 
     useEffect(() => {
+        getAllDisk()
         getDiskSpace('C')
-    }, [getDiskSpace])
+    }, [getAllDisk, getDiskSpace])
 
     return (
         <>
-            { true && <p className="text-red-600">ok</p>}
+            <div className='container mx-auto'>
+                <div className='flex flex-row items-center justify-center space-x-4 px-4 py-2'>
+                    { disks && disks.map((item, index) => {
+                        return (
+                                <div key={index} className='border-2 border-blue-300 bg-blue-200 rounded-xl px-4 py-2'>
+                                    <p>{`${item.fs}`}</p>
+                                    <p>{`Type: ${item.type}`}</p>
+                                    <p>{`Size: ${item.size}`}</p>
+                                    <p>{`Used: ${item.used}`}</p>
+                                    <p>{`Available: ${item.available}`}</p>
+                                    <p>{`Use: ${item.use}%`}</p>
+                                </div>
+                            )
+                        }
+                    )}
+                </div>
+            </div>
         </>
     )
 }
 
 Dashboard.propTypes = {
     getDiskSpace: PropTypes.func.isRequired,
-    // disks: PropTypes.object.isRequired
+    disk: PropTypes.object.isRequired
 }
 
 const mapStateToProps = state => ({
-    disk: state.disk
+    disk: state.disk,
 })
 
-export default connect(mapStateToProps, { getDiskSpace })(Dashboard)
-// export default connect(null, { getDiskSpace })(Dashboard)
+export default connect(mapStateToProps, { getDiskSpace, getAllDisk })(Dashboard)
