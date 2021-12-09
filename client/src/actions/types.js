ed15b31300b600702e9937e46bbf070fcfa9fa54
Mohammad Denny <mohammaddenny94@gmail.com>
2021-12-09 17:21:40 +0700

    set actions

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,2 +1,3 @@
+export const GET_ALL_DISK = 'GET_ALL_DISK'
 export const GET_DISK_SPACE = 'GET_DISK_SPACE'
-export const DISK_SPACE_ERROR = 'DISK_SPACE_ERROR'
+export const DISK_ERROR = 'DISK_ERROR'
