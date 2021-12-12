77b80f2523de446e1ea48a2decffbfd1d0b1b3a0
Mohammad Denny <mohammaddenny94@gmail.com>
2021-12-13 00:37:32 +0700

    add depedency array useeffect

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux'
 import { getAllDisk, getDiskSpace } from '../../actions/disk'
 
 const data = [
-    { 'path': 'E', 'label': 'data' },
+    { 'path': 'D', 'label': 'data' },
     { 'path': 'C', 'label': 'system' },
 ]
 
@@ -13,7 +13,11 @@ const Dashboard = ({ getAllDisk, getDiskSpace, disk: { disk, disks } }) => {
     useEffect(() => {
         getAllDisk()
         getDiskSpace('C')
-    }, [getAllDisk, getDiskSpace])
+    }, [getAllDisk, getDiskSpace, disks])
+
+    const convertByte = (data) => {
+        console.log(toString(data).length)
+    }
 
     return (
         <>
@@ -23,19 +27,35 @@ const Dashboard = ({ getAllDisk, getDiskSpace, disk: { disk, disks } }) => {
                         data && data.map((item, index) => {
                             console.log(disks.filter(filterdisk => filterdisk.mount === `${item.path}:`))
                             return (
-                                <div key={index} className='bg-blue-300 border-2 border-blue-300 rounded-xl'>
-                                    <div className='px-4 py-2'>
-                                        <p>{item.label}</p>
+                                <div key={index}>
+                                    <div className='px-4 py-2 border-2 rounded-t-xl'>
+                                        <p className='uppercase'>{item.label}</p>
                                     </div>
                                     {
                                         disks && disks.filter(filterdisk => filterdisk.mount === `${item.path}:`)
                                             .map((item, index) => {
                                                 return (
-                                                        <div key={index} className='bg-blue-200 rounded-b-xl px-4 py-2'>
-                                                            <p>{`Size: ${item.size}`}</p>
-                                                            <p>{`Used: ${item.used}`}</p>
-                                                            <p>{`Free: ${item.available}`}</p>
-                                                            <p>{`Use: ${item.use}%`}</p>
+                                                        <div key={index} className='flex flex-row border-2 px-4 py-2 rounded-b-xl space-x-4'>
+                                                            <div className='flex flex-row space-x-2'>
+                                                                <div>    
+                                                                    <p>Size:</p>
+                                                                    <p>Used:</p>
+                                                                    <p>Free:</p>
+                                                                </div>
+                                                                <div> 
+                                                                    <p>{item.size}</p>   
+                                                                    <p>{convertByte(item.size)}</p>   
+                                                                    <p>{item.used}</p>
+                                                                    <p>{item.available}</p>
+                                                                </div>
+                                                            </div>
+                                                            <div className='transform -translate-y-11 flex border-2 overflow-hidden w-20 h-auto rounded-full'> 
+                                                                <div className='absolute z-20 bg-white h-1/2 w-full'></div>   
+                                                                <div className='z-30 px-2 flex items-center h-full w-full justify-center'>
+                                                                    <p className='font-bold' >{`${item.use}%`}</p>
+                                                                </div>
+                                                                <div className='absolute z-10 h-full bg-blue-400 w-full'></div>  
+                                                            </div>
                                                         </div>
                                                     )
                                                 }
