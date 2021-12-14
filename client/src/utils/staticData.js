e6c3dc483c9df669118a7768247c242637dc7068
Mohammad Denny <mohammaddenny94@gmail.com>
2021-12-14 18:26:35 +0700

    create data static

---
+++ client/src/utils/staticData.js
@@ -0,0 +1,24 @@
+export const dataDisk = [
+    { 'path': 'Z', 'ip': '192.168.150.214', 'label': 'broadcast'},
+    { 'path': 'Y', 'ip': '192.168.150.213', 'label': 'visualinspector'},
+    { 'path': 'X', 'ip': '172.16.45.225', 'label': 'ftp'},
+    { 'path': 'W', 'ip': '172.16.45.225', 'label': 'watchdalet'},
+    { 'path': 'V', 'ip': '172.16.40.237', 'label': 'export'},
+    { 'path': 'U', 'ip': '172.16.45.22', 'label': 'brio01'},
+    { 'path': 'T', 'ip': '172.16.45.23', 'label': 'brio02'},
+    { 'path': 'S', 'ip': '172.16.45.24', 'label': 'brio03'},
+    { 'path': 'R', 'ip': '172.16.40.158', 'label': 'tobeqc'},
+]
+
+export const dataTicket = [
+    { 'id': '123321', 'name': 'Angga Emirisia', 'date': '2021-12-14T10:12:21+07:00', 'update': '', 'department': 'editing', 'subject': 'editing gs03', 'problem': 'pc editing gs03 ketika dipakai edit tibe-tibe bluescreen', 'solution': '', 'onduty': ['Angga Emirisia', 'Neldo Joseph'], 'note': '', 'status': 'open', 'priority': 'medium', 'history' : ''  },
+    { 'id': '345353', 'name': 'Angga Emirisia', 'date': '2021-12-14T11:30:21+07:00', 'update': '', 'department': 'redaksi', 'subject': 'shortcut sent to dalet di redaksi06', 'problem': 'user redaksi minta dibuatin shortcut sent to dalet', 'solution': 'create shortcut sent to dalet dari pc vip03', 'onduty': ['Angga Emirisia', 'Neldo Joseph'], 'note': '', 'status': 'in progress', 'priority': 'low', 'history' : ''  },
+    { 'id': '345353', 'name': 'Angga Emirisia', 'date': '2021-12-14T11:30:21+07:00', 'update': '', 'department': 'redaksi', 'subject': 'shortcut sent to dalet di redaksi06', 'problem': 'user redaksi minta dibuatin shortcut sent to dalet', 'solution': 'create shortcut sent to dalet dari pc vip03', 'onduty': ['Angga Emirisia', 'Neldo Joseph'], 'note': '', 'status': 'pending', 'priority': 'low', 'history' : ''  },
+    { 'id': '567567', 'name': 'Deni Nurdiana', 'date': '2021-12-14T12:12:21+07:00', 'update': '', 'department': 'mcr', 'subject': 'ntp fw-a itx not sync', 'problem': 'service timesync di fw-a itx tidak sync dengan ntp server 49.123', 'solution': '', 'onduty': ['Deni Nurdiana'], 'note': 'eskalasi ke elnet', 'status': 'escalated', 'priority': 'critical', 'history' : ''  },
+    { 'id': '123321', 'name': 'Angga Emirisia', 'date': '2021-12-14T10:12:21+07:00', 'update': '', 'department': 'editing', 'subject': 'editing gs03', 'problem': 'pc editing gs03 ketika dipakai edit tibe-tibe bluescreen', 'solution': '', 'onduty': ['Angga Emirisia', 'Neldo Joseph'], 'note': '', 'status': 'in progress', 'priority': 'medium', 'history' : ''  },
+    { 'id': '345353', 'name': 'Angga Emirisia', 'date': '2021-12-14T11:30:21+07:00', 'update': '', 'department': 'redaksi', 'subject': 'shortcut sent to dalet di redaksi06', 'problem': 'user redaksi minta dibuatin shortcut sent to dalet', 'solution': 'create shortcut sent to dalet dari pc vip03', 'onduty': ['Angga Emirisia', 'Neldo Joseph'], 'note': '', 'status': 'on hold', 'priority': 'low', 'history' : ''  },
+    { 'id': '567567', 'name': 'Deni Nurdiana', 'date': '2021-12-14T12:12:21+07:00', 'update': '', 'department': 'mcr', 'subject': 'ntp fw-a itx not sync', 'problem': 'service timesync di fw-a itx tidak sync dengan ntp server 49.123', 'solution': '', 'onduty': ['Deni Nurdiana'], 'note': 'eskalasi ke elnet', 'status': 'escalated', 'priority': 'critical', 'history' : ''  },
+    { 'id': '123321', 'name': 'Angga Emirisia', 'date': '2021-12-14T10:12:21+07:00', 'update': '', 'department': 'editing', 'subject': 'editing gs03', 'problem': 'pc editing gs03 ketika dipakai edit tibe-tibe bluescreen', 'solution': '', 'onduty': ['Angga Emirisia', 'Neldo Joseph'], 'note': '', 'status': 'in progress', 'priority': 'medium', 'history' : ''  },
+    { 'id': '345353', 'name': 'Angga Emirisia', 'date': '2021-12-14T11:30:21+07:00', 'update': '', 'department': 'redaksi', 'subject': 'shortcut sent to dalet di redaksi06', 'problem': 'user redaksi minta dibuatin shortcut sent to dalet', 'solution': 'create shortcut sent to dalet dari pc vip03', 'onduty': ['Angga Emirisia', 'Neldo Joseph'], 'note': '', 'status': 'solved', 'priority': 'low', 'history' : ''  },
+    { 'id': '567567', 'name': 'Deni Nurdiana', 'date': '2021-12-14T12:12:21+07:00', 'update': '', 'department': 'mcr', 'subject': 'ntp fw-a itx not sync', 'problem': 'service timesync di fw-a itx tidak sync dengan ntp server 49.123', 'solution': '', 'onduty': ['Deni Nurdiana'], 'note': 'eskalasi ke elnet', 'status': 'closed', 'priority': 'critical', 'history' : ''  },
+]
