59e9331e69569b81d9d859ed3a099df4240327d6
Mohammad Denny <mohammaddenny94@gmail.com>
2021-12-20 19:59:47 +0700

    replace action disk

--- client/src/components/monitoring/Monitoring.js
+++ client/src/components/monitoring/Monitoring.js
@@ -1,27 +1,26 @@
 import React, { useEffect } from 'react'
-import { dataDisk } from '../../utils/staticData'
 import MonitoringCard from './MonitoringCard'
 import MonitoringCardItem from './MonitoringCardItem'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { getAllDisk, getDiskSpace } from '../../actions/disk'
+import { getSavedDisk, getMappingDisk } from '../../actions/disk'
 
-const Monitoring = ({ getAllDisk, getDiskSpace, disk: { disk, disks } }) => {
+const Monitoring = ({ getSavedDisk, getMappingDisk, disk: { savedDisk, mappingDisk } }) => {
 
     useEffect(() => {
-        getAllDisk()
-        // getDiskSpace('C')
-    }, [getAllDisk, getDiskSpace, disks])
+        getSavedDisk()
+        getMappingDisk()
+    }, [getSavedDisk, getMappingDisk, mappingDisk])
 
     return (
         <>
             <div className='grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-4 w-full'>
                 {
-                    dataDisk && dataDisk.map((item, index) => {
+                    savedDisk && savedDisk.map((item, index) => {
                         return (
                             <MonitoringCard key={index} data={item}>
                                 {
-                                    disks && disks.filter(filterdisk => filterdisk.mount === `${item.path}:`)
+                                    mappingDisk && mappingDisk.filter(filterdisk => filterdisk.mount === `${item.path}:`)
                                         .map((item, index) => {
                                             return (
                                                     <MonitoringCardItem key={index} data={item} />
@@ -40,12 +39,12 @@ const Monitoring = ({ getAllDisk, getDiskSpace, disk: { disk, disks } }) => {
 }
 
 Monitoring.propTypes = {
-    getDiskSpace: PropTypes.func.isRequired,
+    getMappingDisk: PropTypes.func.isRequired,
     disk: PropTypes.object.isRequired
 }
 
 const mapStateToProps = state => ({
-    disk: state.disk,
+    disk: state.disk
 })
 
-export default connect(mapStateToProps, { getDiskSpace, getAllDisk })(Monitoring)
+export default connect(mapStateToProps, { getMappingDisk, getSavedDisk })(Monitoring)
