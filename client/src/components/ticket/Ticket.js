e5e62cd1394339da56f325eef78ae573bf30561b
Mohammad Denny <mohammaddenny94@gmail.com>
2021-12-20 19:59:21 +0700

    import redux and connect

--- client/src/components/ticket/Ticket.js
+++ client/src/components/ticket/Ticket.js
@@ -1,24 +1,30 @@
 import React, { useState, useEffect } from 'react'
-import { dataTicket } from '../../utils/staticData'
 import moment from 'moment'
 import classnames from 'classnames'
 import Modal from '../modal/Modal'
+import TicketForm from './TicketForm'
 import TicketDetail from './TicketDetail'
 import TicketHistory from './TicketHistory'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { getTickets, getTicket } from '../../actions/ticket'
 
-const Ticket = () => {
+const Ticket = ({ getTickets, getTicket, ticket: { tickets, ticket } }) => {
     const [ term, setTerm ] = useState()
-    const [ filterTicket, setFilterTicket ] = useState()
-    const [ ticketModalDetail, setTicketModalDetail ] = useState(false)
-    const [ ticketModalHistory, setTicketModalHistory ] = useState(false)
+    const [ data, setData ] = useState(tickets)
+    const [ callModalForm, setCallModalForm ] = useState(false)
+    const [ callModalDetail, setCallModalDetail ] = useState(false)
+    const [ callModalHistory, setCallModalHistory ] = useState(false)
     const [ dataModal, setDataModal ] = useState()
 
-    const filterByStatus = (data, term) => {
-        if ( !term ) {
-            setFilterTicket(data)
-        } else {
-            setFilterTicket(data.filter(item => item.status === term))
-        }
+    useEffect(() => {
+        getTickets()
+        getTicket(term)
+    }, [getTickets, getTicket, tickets, term])
+
+    const filterTicket = (term) => {
+        setData(term)
+        setData(ticket)
     }
 
     const countTicketByTerm = (data, term) => {
@@ -26,18 +32,19 @@ const Ticket = () => {
         return count.length
     }
 
-    useEffect(() => {
-        filterByStatus(dataTicket, term)
-    })
-
-    const handleTicketModalHistory = (dataTicket, itemTicket) => {
-        setTicketModalHistory(true)
-        setDataModal({'dataTicket': dataTicket, 'itemTicket': itemTicket})
+    const handleCallModalForm = (title, data, items) => {
+        setCallModalForm(true)
+        setDataModal({'title': title, 'data': data, 'items': items})
     }
 
-    const handleTicketModalDetail = (dataTicket, itemTicket) => {
-        setTicketModalDetail(true)
-        setDataModal({'dataTicket': dataTicket, 'itemTicket': itemTicket})
+    const handleCallModalHistory = (title, data, items) => {
+        setCallModalHistory(true)
+        setDataModal({'title': title, 'data': data, 'items': items})
+    }
+
+    const handleCallModalDetail = (title, data, items) => {
+        setCallModalDetail(true)
+        setDataModal({'title': title, 'data': data, 'items': items})
     }
 
     return (
@@ -45,7 +52,10 @@ const Ticket = () => {
             <div className='relative z-0 space-y-4 h-full w-full'>
                 <div className='flex flex-col items-center justify-center py-4 bg-white h-auto w-full '>
                     <div className='flex flex-row items-center justify-center h-12 space-x-4 w-full py-2'>
-                        <button className='flex flex-row h-full items-center space-x-2 justify-center px-2 bg-blue-400 rounded-xl hover:bg-blue-500'>
+                        <button 
+                            className='flex flex-row h-full items-center space-x-2 justify-center px-2 bg-blue-400 rounded-xl hover:bg-blue-500'
+                            onClick={() => handleCallModalForm('Create New Ticket')}
+                        >
                             <svg xmlns="http://www.w3.org/2000/svg" className="text-white py-1 h-full w-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                             </svg>
@@ -63,7 +73,7 @@ const Ticket = () => {
                             className={classnames('bg-white text-gray-800 px-4 py-1 border-2  rounded-xl hover:bg-gray-300', {
                             'bg-gray-300 text-gray-900 font-bold shadow-md' : !term
                             })} 
-                            onClick={() => setTerm()}
+                            onClick={() => setData()}
                         >
                             All
                         </button>
@@ -71,57 +81,57 @@ const Ticket = () => {
                             className={classnames('bg-yellow-200 text-yellow-800 px-4 py-1 rounded-xl hover:bg-yellow-300', {
                             'bg-yellow-300 text-yellow-900 font-bold shadow-md' : term === 'open'
                             })} 
-                            onClick={() => setTerm('open')}
+                            onClick={() => setData('open')}
                         >   
-                            Open { countTicketByTerm(dataTicket, 'open') && <sup>{`${countTicketByTerm(dataTicket, 'open')}`}</sup> }
+                            Open { countTicketByTerm(tickets, 'open') && <sup>{`${countTicketByTerm(tickets, 'open')}`}</sup> }
                         </button>
                         <button 
                             className={classnames('bg-blue-200 text-blue-800 px-4 py-1 rounded-xl hover:bg-blue-300', {
                             'bg-blue-300 text-blue-900 font-bold shadow-md' : term === 'in progress'
                             })} 
-                            onClick={() => setTerm('in progress')}
+                            onClick={() => setData('in progress')}
                         >   
-                            In Progress { countTicketByTerm(dataTicket, 'in progress') && <sup>{`${countTicketByTerm(dataTicket, 'in progress')}`}</sup> }
+                            In Progress { countTicketByTerm(tickets, 'in progress') && <sup>{`${countTicketByTerm(tickets, 'in progress')}`}</sup> }
                         </button>
                         <button 
                             className={classnames('bg-red-200 text-red-800 px-4 py-1 rounded-xl hover:bg-red-300', {
                             'bg-red-300 text-red-900 font-bold shadow-md' : term === 'pending'
                             })} 
-                            onClick={() => setTerm('pending')}
+                            onClick={() => setData('pending')}
                         >   
-                            Pending { countTicketByTerm(dataTicket, 'pending') && <sup>{`${countTicketByTerm(dataTicket, 'pending')}`}</sup> }
+                            Pending { countTicketByTerm(tickets, 'pending') && <sup>{`${countTicketByTerm(tickets, 'pending')}`}</sup> }
                         </button>
                         <button 
                             className={classnames('bg-pink-200 text-pink-800 px-4 py-1 rounded-xl hover:bg-pink-300', {
                             'bg-pink-300 text-pink-900 font-bold shadow-md' : term === 'on hold'
                             })} 
-                            onClick={() => setTerm('on hold')}
+                            onClick={() => setData('on hold')}
                         >   
-                            On Hold { countTicketByTerm(dataTicket, 'on hold') && <sup>{`${countTicketByTerm(dataTicket, 'on hold')}`}</sup> }
+                            On Hold { countTicketByTerm(tickets, 'on hold') && <sup>{`${countTicketByTerm(tickets, 'on hold')}`}</sup> }
                         </button>
                         <button 
                             className={classnames('bg-purple-200 text-purple-800 px-4 py-1 rounded-xl hover:bg-purple-300', {
                             'bg-purple-300 text-purple-900 font-bold shadow-md' : term === 'escalated'
                             })} 
-                            onClick={() => setTerm('escalated')}
+                            onClick={() => setData('escalated')}
                         >   
-                            Escalated { countTicketByTerm(dataTicket, 'escalated') && <sup>{`${countTicketByTerm(dataTicket, 'escalated')}`}</sup> }
+                            Escalated { countTicketByTerm(tickets, 'escalated') && <sup>{`${countTicketByTerm(tickets, 'escalated')}`}</sup> }
                         </button>
                         <button 
                             className={classnames('bg-green-200 text-green-800 px-4 py-1 rounded-xl hover:bg-green-300', {
                             'bg-green-300 text-green-900 font-bold shadow-md' : term === 'solved'
                             })} 
-                            onClick={() => setTerm('solved')}
+                            onClick={() => setData('solved')}
                         >   
-                            Solved { countTicketByTerm(dataTicket, 'solved') && <sup>{`${countTicketByTerm(dataTicket, 'solved')}`}</sup> }
+                            Solved { countTicketByTerm(tickets, 'solved') && <sup>{`${countTicketByTerm(tickets, 'solved')}`}</sup> }
                         </button>
                         <button 
                             className={classnames('bg-gray-200 text-gray-800 px-4 py-1 rounded-xl hover:bg-gray-300', {
                             'bg-gray-300 text-gray-900 font-bold shadow-md' : term === 'closed'
                             })} 
-                            onClick={() => setTerm('closed')}
+                            onClick={() => setData('closed')}
                         >   
-                            Closed { countTicketByTerm(dataTicket, 'closed') && <sup>{`${countTicketByTerm(dataTicket, 'closed')}`}</sup> }
+                            Closed { countTicketByTerm(tickets, 'closed') && <sup>{`${countTicketByTerm(tickets, 'closed')}`}</sup> }
                         </button>
                     </div>
                 </div>
@@ -169,12 +179,12 @@ const Ticket = () => {
                                     </thead>
                                     <tbody className="bg-white divide-y divide-gray-200">
                                         {
-                                            filterTicket && filterTicket.map((item, index) => {
+                                            data && data.map((item, index) => {
                                                 return (
                                                     <tr key={index}>
                                                         <td className="px-6 py-4 whitespace-nowrap">
                                                             <div className='ml-4'>
-                                                                <button className="text-sm text-gray-900 font-bold uppercase hover:text-gray-700" onClick={() => handleTicketModalDetail(item, item.history)}>{item.subject}</button>
+                                                                <button className="text-sm text-gray-900 font-bold uppercase hover:text-gray-700" onClick={() => handleCallModalDetail(item.subject, item, item.history)}>{item.subject}</button>
                                                                 <div className="text-sm text-gray-500 capitalize">{item.department}</div>
                                                             </div>
                                                         </td>
@@ -207,7 +217,7 @@ const Ticket = () => {
                                                         <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 capitalize">{item.priority}</td>
                                                         <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900 ">{moment(item.date).format('D-M-YYYY, H:mm')}</td>
                                                         <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium space-x-2">
-                                                            <button className="text-indigo-600 hover:text-indigo-900" onClick={() => handleTicketModalHistory(item, item.history)}>
+                                                            <button className="text-indigo-600 hover:text-indigo-900" onClick={() => handleCallModalHistory(item.subject, item, item.history)}>
                                                                 History
                                                             </button>
                                                             <button className="text-indigo-600 hover:text-indigo-900">
@@ -229,23 +239,39 @@ const Ticket = () => {
                 </div>
             </div>
             { 
-                ticketModalDetail && 
+                callModalForm && 
                     ( 
-                        <Modal data={dataModal} onClick={setTicketModalDetail}>
-                            <TicketDetail data={dataModal}/>
-                        </Modal> 
+                        <Modal data={dataModal} onClick={setCallModalForm}/>
+                        //     <TicketForm data={dataModal}/>
+                        // </Modal> 
+                    )
+            }
+            { 
+                callModalDetail && 
+                    ( 
+                        <Modal data={dataModal} onClick={setCallModalDetail}/>
+                        //     <TicketDetail data={dataModal}/>
+                        // </Modal>
                     )
             }
             {   
-                ticketModalHistory && 
+                callModalHistory && 
                     (
-                        <Modal data={dataModal} onClick={setTicketModalHistory}>
-                            <TicketHistory data={dataModal}/>
-                        </Modal>
+                        <Modal data={dataModal} onClick={setCallModalHistory}/>
+                        //     <TicketHistory data={dataModal}/>
+                        // </Modal>
                     ) 
             }
         </>
     )
 }
+Ticket.propTypes = {
+    getTickets: PropTypes.func.isRequired,
+    ticket: PropTypes.object.isRequired
+}
 
-export default Ticket
+const mapStateToProps = state => ({
+    ticket: state.ticket
+})
+
+export default connect(mapStateToProps, { getTickets, getTicket })(Ticket)
