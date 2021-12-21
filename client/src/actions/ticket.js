e4653ee93501b16144a568c8ac00c53674909784
Mohammad Denny <mohammaddenny94@gmail.com>
2021-12-21 16:51:51 +0700

    import action

--- client/src/actions/ticket.js
+++ client/src/actions/ticket.js
@@ -1,8 +1,15 @@
 import { dataTicket } from "../utils/staticData"
-import { GET_TICKETS, GET_TICKET, TICKET_ERROR } from "./types"
+import { 
+    GET_TICKETS,
+    GET_TICKET,
+    TICKET_ERROR,
+    CLEAR_TICKET
+} from "./types"
 
 //  get all ticket
 export const getTickets = () => async dispatch => {
+    dispatch({ type: CLEAR_TICKET })
+
     try {
         const res = await dataTicket
 
@@ -19,15 +26,33 @@ export const getTickets = () => async dispatch => {
     }
 }
 
-//  get ticket
-export const getTicket = (term) => async dispatch => {
+//  get ticket by id
+export const getTicketById = id => async dispatch => {
     try {
         const res = await dataTicket
 
         dispatch({
             type: GET_TICKET,
             // payload: res.data
-            payload: res.filter(item => item.status === term)
+            payload: res.filter(item => item.id === id)
+        })
+    } catch (err) {
+        dispatch({
+            type: TICKET_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        })
+    }
+}
+
+//  get ticket by id
+export const getTicketByStatus = status => async dispatch => {
+    try {
+        const res = await dataTicket
+
+        dispatch({
+            type: GET_TICKET,
+            // payload: res.data
+            payload: res.filter(item => item.status === status)
         })
     } catch (err) {
         dispatch({
