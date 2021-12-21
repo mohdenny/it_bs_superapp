fca1396fccb77d288b8a024504712d7e0aa63a72
Mohammad Denny <mohammaddenny94@gmail.com>
2021-12-21 16:51:43 +0700

    add type

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,6 +1,7 @@
-export const GET_SAVED_DISK = 'GET_SAVED_DISK'
+export const GET_SAVED_DISK_PATH = 'GET_SAVED_DISK_PATH'
 export const GET_MAPPING_DISK = 'GET_MAPPING_DISK'
 export const DISK_ERROR = 'DISK_ERROR'
 export const GET_TICKETS = 'GET_TICKETS'
 export const GET_TICKET = 'GET_TICKET'
+export const CLEAR_TICKET = 'CLEAR_TICKET'
 export const TICKET_ERROR = 'TICKET_ERROR'
