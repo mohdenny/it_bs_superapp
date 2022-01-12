bab73f375118527324fb5e15dbcdc6ddba49144b
Mohammad Denny <mohammaddenny94@gmail.com>
2022-01-12 16:15:59 +0700

    create comp sidebar

--- client/src/components/layout/Sidebar.js
+++ client/src/components/layout/Sidebar.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import classnames from 'classnames'
+import ButtonSidebar from '../button/ButtonSidebar'
 
 
 const Sidebar = ({ children }) => {
@@ -39,31 +40,12 @@ const Sidebar = ({ children }) => {
                         {
                             !toggleMenu &&
                             <div className="flex flex-col bg-white h-full w-64 py-4">
-                                <Link 
-                                    className={classnames("hover:bg-gray-200 h-10 w-full flex px-8 space-x-4 content-center", {
-                                        'bg-gray-300' : location.pathname === '/dashboard'
-                                    })}
-                                    to={'/dashboard'}
-                                    aria-current="page"
-                                >
-                                    {
-                                        location.pathname === '/dashboard' ? (
-                                            <svg xmlns="http://www.w3.org/2000/svg" className="h-full w-6" viewBox="0 0 20 20" fill="currentColor">
-                                                <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
-                                            </svg>
-                                        ) : (
-                                            <svg xmlns="http://www.w3.org/2000/svg" className="h-full w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-                                            </svg>
-                                        )
-                                    }
-                                    
-                                    <button className={classnames("h-full w-auto", {
-                                        'font-bold' : location.pathname === '/dashboard'
-                                    })}>
-                                        Home
-                                    </button>
-                                </Link>
+                                <ButtonSidebar
+                                    icon='home'
+                                    label='Home'
+                                    location={location.pathname}
+                                    path='/dashboard'
+                                />
                                 <Link 
                                     className={classnames("hover:bg-gray-200 h-10 w-full flex px-8 space-x-4 content-center", {
                                         'bg-gray-300' : location.pathname === '/explore'
