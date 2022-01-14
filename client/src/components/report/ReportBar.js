d504fceb98db33846ee74f3b8b8aaa8758550d6d
Mohammad Denny <mohammaddenny94@gmail.com>
2022-01-14 23:12:03 +0700

    change paragraph

--- client/src/components/report/ReportBar.js
+++ client/src/components/report/ReportBar.js
@@ -51,7 +51,7 @@ const ReportBar = ({  isActiveTab, setIsActiveTab }) => {
                     })} 
                     onClick={() => setIsActiveTab('live-report')}
                 >
-                    Live Report
+                    Live
                 </button>
             </div>
             <div className='flex flex-row items-center justify-center px-4 h-12 space-x-4 w-full py-2'>
@@ -65,19 +65,19 @@ const ReportBar = ({  isActiveTab, setIsActiveTab }) => {
                                 <svg xmlns="http://www.w3.org/2000/svg" className="text-white py-1 h-full w-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                                 </svg>
-                                <p className='text-white'>Create Program Report</p>
+                                <p className='text-white'>Create New</p>
                             </button>
                         )
                         :
                         (
                             <button 
-                                className='flex flex-row h-full items-center space-x-2 justify-center px-2 bg-blue-400 rounded-xl hover:bg-blue-500'
+                                className='flex flex-row h-full items-center space-x-2 justify-center px-2 bg-green-400 rounded-xl hover:bg-green-500'
                                 
                             >
                                 <svg xmlns="http://www.w3.org/2000/svg" className="text-white py-1 h-full w-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                                 </svg>
-                                <p className='text-white'>Create Live Report</p>
+                                <p className='text-white'>Create New</p>
                             </button>
                         )
                 } 
