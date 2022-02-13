d64c82fb5ed68d743359097f6bb1bae96bccc85b
Mohammad Denny <mohammaddenny94@gmail.com>
2022-02-14 05:43:02 +0700

    import globalfilter

--- client/src/components/ticket/TicketBar.js
+++ client/src/components/ticket/TicketBar.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import { Form, Formik } from 'formik'
-import * as Yup from 'yup'
-import FormControl from '../form/FormControl'
 import ButtonControl from '../button/ButtonControl'
+import { GlobalFilter } from '../table/GlobalFilter'
 
 const valuesButtonGeneral = [
     { name: '', control: 'general', label: 'All' },
@@ -15,10 +13,6 @@ const valuesButtonGeneral = [
     { name: 'closed', control: 'general', label: 'Closed' }
 ]
 
-const initialValues = {
-    search: ''
-}
-
 const TicketBar = ({ 
     ticketToCount, 
     countTicketByStatus, 
@@ -26,34 +20,12 @@ const TicketBar = ({
     isActiveFilterStatus, 
     setIsActiveFilterStatus 
 }) => {
-    const validationSchema = Yup.object({
-        search: Yup.string()
-    })
-
-    const onSubmit = values => {
-        console.log('Form data', values)
-        console.log('Saved data', JSON.parse(JSON.stringify(values)))
-    }
-
     return (
         <div className='flex flex-col items-center justify-center py-4 bg-white h-auto w-full '>
-            <div className='flex items-center justify-center h-12 w-full py-2'>
-                <Formik
-                    initialValues={initialValues}
-                    validationSchema={validationSchema}
-                    onSubmit={onSubmit}
-                >
-                    <Form 
-                        className='flex flex-row h-full items-center justify-center'
-                    >
-                        <FormControl
-                            control={'search'}
-                            name={'search'}
-                        />
-                    </Form>
-                </Formik>
+            <div className='flex items-center justify-center h-auto w-full py-2'>
+                <GlobalFilter/>
             </div>
-            <div className='flex flex-row items-center justify-center h-12 space-x-4 w-full'>
+            <div className='flex flex-row items-center justify-center h-auto space-x-4 w-full'>
                 {
                     valuesButtonGeneral.map((item, index) => {
                         return (
@@ -71,16 +43,13 @@ const TicketBar = ({
                     })
                 }
             </div>
-            <div className='flex items-center justify-center h-12 space-x-4 w-full py-2'>
-                <button 
-                    className='flex flex-row h-full items-center space-x-2 justify-center px-2 bg-blue-400 rounded-xl hover:bg-blue-500'
-                    onClick={() => handleCallModal('modal-create-form')}
-                >
-                    <svg xmlns="http://www.w3.org/2000/svg" className="text-white py-1 h-full w-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-                    </svg>
-                    <p className='text-white'>Create New</p>
-                </button>
+            <div className='flex items-center justify-center h-auto space-x-4 w-full py-2'>
+                <ButtonControl
+                    control='primary'
+                    label='Create New'
+                    icon='search'
+                    onClick={handleCallModal}
+                />
             </div>
         </div>
     )
