de3c23d307cff97efb3a61b46ea4708979fe68a2
Mohammad Denny <mohammaddenny94@gmail.com>
2022-02-14 05:41:19 +0700

    change height

--- client/src/components/button/ButtonGeneral.js
+++ client/src/components/button/ButtonGeneral.js
@@ -4,7 +4,7 @@ import classnames from 'classnames'
 const ButtonGeneral = ({ name, label, isActiveFilterStatus, setIsActiveFilterStatus, ticketToCount, countTicketByStatus }) => {
     return (
         <button 
-            className={classnames('bg-white text-gray-800 px-4 py-1 border-2 rounded-xl hover:bg-gray-100', {
+            className={classnames('bg-white h-11 text-gray-800 px-4 py-1 border-2 rounded-xl hover:bg-gray-100', {
             'bg-gray-100 text-gray-900 font-bold shadow-md' : isActiveFilterStatus === name
             })} 
             onClick={() => setIsActiveFilterStatus(name)}
