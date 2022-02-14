67a993ba1749e7b2b221b1b1634c4a148c3800d0
Mohammad Denny <mohammaddenny94@gmail.com>
2022-02-14 17:02:16 +0700

    import usestorby, usepagination

--- client/src/components/table/Table.js
+++ client/src/components/table/Table.js
@@ -1,50 +1,63 @@
 import React, { useMemo } from 'react'
-import { useTable, useGlobalFilter } from 'react-table'
+import { useTable, useSortBy ,useGlobalFilter, usePagination } from 'react-table'
 import { COLUMNS } from './columns'
 import { GlobalFilter } from './GlobalFilter'
 
-export const Table = ({ datas, handleCallModal }) => {
+export const Table = ({ datas }) => {
     const columns = useMemo(() => COLUMNS, [])
     const data = useMemo(() => datas, [datas])
-  
+
     const {
         getTableProps,
         getTableBodyProps,
         headerGroups,
-        footerGroups,
-        rows,
-        prepareRow,
+        page,
+        nextPage,
+        previousPage,
+        canPreviousPage,
+        canNextPage,
+        pageOptions,
         state,
+        gotoPage,
+        pageCount,
+        setPageSize,
+        prepareRow,
         setGlobalFilter
     } = useTable(
         {
-            columns,
-            data
+        columns,
+        data
         },
-        useGlobalFilter
+        useGlobalFilter,
+        useSortBy,
+        usePagination
     )
-  
-    const { globalFilter } = state
-  
+
+    const { pageIndex, pageSize, globalFilter } = state
+
     return (
         <>
             <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
-            <table 
-                className="min-w-full divide-y divide-gray-200"
-                {...getTableProps()}
-            >
+            <table className="min-w-full divide-y divide-gray-200" {...getTableProps()}>
                 <thead className="bg-gray-50">
                     {headerGroups.map(headerGroup => (
                         <tr {...headerGroup.getHeaderGroupProps()}>
-                        {headerGroup.headers.map(column => (
-                            <th 
-                                scope="col"
-                                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider "
-                                {...column.getHeaderProps()}
-                            >
-                            {column.render('Header')}
-                            </th>
-                        ))}
+                            {headerGroup.headers.map(column => (
+                                <th 
+                                    scope="col"
+                                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider "
+                                    {...column.getHeaderProps(column.getSortByToggleProps())}
+                                >
+                                    {column.render('Header')}
+                                    <span>
+                                        {column.isSorted
+                                        ? column.isSortedDesc
+                                            ? ' 🔽'
+                                            : ' 🔼'
+                                        : ''}
+                                    </span>
+                                </th>
+                            ))}
                         </tr>
                     ))}
                 </thead>
@@ -52,32 +65,66 @@ export const Table = ({ datas, handleCallModal }) => {
                     className="bg-white divide-y divide-gray-200"
                     {...getTableBodyProps()}
                 >
-                    {rows.map(row => {
+                    {page.map(row => {
                         prepareRow(row)
                         return (
-                        <tr {...row.getRowProps()}>
-                            {row.cells.map(cell => {
-                            return  <td 
-                                        className="px-6 py-4 whitespace-nowrap"
-                                        {...cell.getCellProps()}>{cell.render('Cell')}
-
-                                    </td>
-                                    {/* YANG INI */}
-                            })}
-                        </tr>
+                            <tr {...row.getRowProps()}>
+                                {row.cells.map(cell => {
+                                    return (
+                                        <td 
+                                            className="px-6 py-4 whitespace-nowrap" 
+                                            {...cell.getCellProps()}
+                                        >
+                                            {cell.render('Cell')}
+                                        </td>
+                                    )
+                                })}
+                            </tr>
                         )
                     })}
                 </tbody>
-                    <tfoot>
-                        {footerGroups.map(footerGroup => (
-                            <tr {...footerGroup.getFooterGroupProps()}>
-                            {footerGroup.headers.map(column => (
-                                <td {...column.getFooterProps()}>{column.render('Footer')}</td>
-                            ))}
-                            </tr>
-                        ))}
-                    </tfoot>
             </table>
+            <div>
+                <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+                    {'<<'}
+                </button>{' '}
+                <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+                    Previous
+                </button>{' '}
+                <button onClick={() => nextPage()} disabled={!canNextPage}>
+                    Next
+                </button>{' '}
+                <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+                    {'>>'}
+                </button>{' '}
+                <span>
+                    Page{' '}
+                    <strong>
+                        {pageIndex + 1} of {pageOptions.length}
+                    </strong>{' '}
+                </span>
+                <span>
+                    | Go to page:{' '}
+                    <input
+                        type='number'
+                        defaultValue={pageIndex + 1}
+                        onChange={e => {
+                            const pageNumber = e.target.value ? Number(e.target.value) - 1 : 0
+                            gotoPage(pageNumber)
+                        }}
+                        style={{ width: '50px' }}
+                    />
+                </span>{' '}
+                <select
+                    value={pageSize}
+                    onChange={e => setPageSize(Number(e.target.value))}>
+                        {[10, 25, 50].map(pageSize => (
+                        <option key={pageSize} value={pageSize}>
+                            Show {pageSize}
+                        </option>
+                    ))}
+                </select>
+            </div>
         </>
     )
-  }
+}
