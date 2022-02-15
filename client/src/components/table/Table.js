362c60990fef55a990c84d78dd4c014ee6bda523
Mohammad Denny <mohammaddenny94@gmail.com>
2022-02-15 17:41:26 +0700

    import pagination and button control

--- client/src/components/table/Table.js
+++ client/src/components/table/Table.js
@@ -1,10 +1,11 @@
 import React, { useMemo } from 'react'
-import { useTable, useSortBy ,useGlobalFilter, usePagination } from 'react-table'
-import { COLUMNS } from './columns'
-import { GlobalFilter } from './GlobalFilter'
+import { useTable, useSortBy ,useGlobalFilter ,usePagination } from 'react-table'
+import FormControl from '../form/FormControl'
+import Pagination from './Pagination'
+import ButtonControl from '../button/ButtonControl'
 
-export const Table = ({ datas, handleCallModal }) => {
-    const columns = useMemo(() => COLUMNS, [])
+export const Table = ({ columnsTable, datas, onClick, BadgeControl }) => {
+    const columns = useMemo(() => columnsTable, [columnsTable])
     const data = useMemo(() => datas, [datas])
 
     const {
@@ -36,95 +37,76 @@ export const Table = ({ datas, handleCallModal }) => {
     const { pageIndex, pageSize, globalFilter } = state
 
     return (
-        <>
-            <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
-            <table className="min-w-full divide-y divide-gray-200" {...getTableProps()}>
-                <thead className="bg-gray-50">
-                    {headerGroups.map(headerGroup => (
-                        <tr {...headerGroup.getHeaderGroupProps()}>
-                            {headerGroup.headers.map(column => (
-                                <th 
-                                    scope="col"
-                                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider "
-                                    {...column.getHeaderProps(column.getSortByToggleProps())}
-                                >
-                                    {column.render('Header')}
-                                    <span>
-                                        {column.isSorted
-                                        ? column.isSortedDesc
-                                            ? ' 🔽'
-                                            : ' 🔼'
-                                        : ''}
-                                    </span>
-                                </th>
-                            ))}
-                        </tr>
-                    ))}
-                </thead>
-                <tbody 
-                    className="bg-white divide-y divide-gray-200"
-                    {...getTableBodyProps()}
-                >
-                    {page.map(row => {
-                        prepareRow(row)
-                        return (
-                            <tr {...row.getRowProps()}>
-                                {row.cells.map(cell => {
-                                    return (
-                                        <td 
-                                            className="px-6 py-4 whitespace-nowrap" 
-                                            {...cell.getCellProps()}
-                                        >
-                                            {cell.render('Cell', {onClick: handleCallModal})}
-                                        </td>
-                                    )
-                                })}
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
-            <div>
-                <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
-                    {'<<'}
-                </button>{' '}
-                <button onClick={() => previousPage()} disabled={!canPreviousPage}>
-                    Previous
-                </button>{' '}
-                <button onClick={() => nextPage()} disabled={!canNextPage}>
-                    Next
-                </button>{' '}
-                <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
-                    {'>>'}
-                </button>{' '}
-                <span>
-                    Page{' '}
-                    <strong>
-                        {pageIndex + 1} of {pageOptions.length}
-                    </strong>{' '}
-                </span>
-                <span>
-                    | Go to page:{' '}
-                    <input
-                        type='number'
-                        defaultValue={pageIndex + 1}
-                        onChange={e => {
-                            const pageNumber = e.target.value ? Number(e.target.value) - 1 : 0
-                            gotoPage(pageNumber)
-                        }}
-                        style={{ width: '50px' }}
-                    />
-                </span>{' '}
-                <select
-                    value={pageSize}
-                    onChange={e => setPageSize(Number(e.target.value))}>
-                        {[10, 25, 50].map(pageSize => (
-                        <option key={pageSize} value={pageSize}>
-                            Show {pageSize}
-                        </option>
-                    ))}
-                </select>
-            </div>
-        </>
+        <div className='space-y-4'>
+            <FormControl control='globalfilter' filter={globalFilter} setFilter={setGlobalFilter} />
+            { page &&
+                (
+                    <>
+                        <table className="min-w-full divide-y border-2 divide-gray-200 table-fixed" {...getTableProps()}>
+                            <thead className="bg-gray-50">
+                                {
+                                    headerGroups.map(headerGroup => (
+                                        <tr {...headerGroup.getHeaderGroupProps()}>
+                                            {headerGroup.headers.map(column => (
+                                                <th 
+                                                    scope="col"
+                                                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                                    {...column.getHeaderProps(column.getSortByToggleProps())}
+                                                >
+                                                    {column.render('Header')}
+                                                    <span>
+                                                        {column.isSorted
+                                                        ? column.isSortedDesc
+                                                            ? ' 🔽'
+                                                            : ' 🔼'
+                                                        : ''}
+                                                    </span>
+                                                </th>
+                                            ))}
+                                        </tr>
+                                    ))
+                                }
+                            </thead>
+                            <tbody 
+                                className="bg-white divide-y divide-gray-200"
+                                {...getTableBodyProps()}
+                            >
+                                {
+                                    page.map(row => {
+                                        prepareRow(row)
+                                        return (
+                                            <tr {...row.getRowProps()}>
+                                                {row.cells.map(cell => {
+                                                    return (
+                                                        <td 
+                                                            className="px-6 py-4 capitalize" 
+                                                            {...cell.getCellProps()}
+                                                        >
+                                                            {cell.render('Cell', {onClick: onClick, BadgeControl: BadgeControl, ButtonControl: ButtonControl})}
+                                                        </td>
+                                                    )
+                                                })}
+                                            </tr>
+                                        )
+                                    })
+                                }
+                            </tbody>
+                        </table>
+                        <Pagination 
+                            gotoPage={gotoPage}
+                            previousPage={previousPage}
+                            nextPage={nextPage}
+                            canPreviousPage={canPreviousPage}
+                            canNextPage={canNextPage}
+                            pageIndex={pageIndex}
+                            pageCount={pageCount}
+                            pageOptions={pageOptions}
+                            setPageSize={setPageSize}
+                            pageSize={pageSize}
+                        />
+                    </>
+                )
+            }
+        </div>
     )
 }
