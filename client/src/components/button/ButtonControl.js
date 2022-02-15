780cab4a195c983addf7de85a12394735137845a
Mohammad Denny <mohammaddenny94@gmail.com>
2022-02-15 17:39:53 +0700

    change import button general to secondary

--- client/src/components/button/ButtonControl.js
+++ client/src/components/button/ButtonControl.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import ButtonGeneral from './ButtonGeneral'
+import ButtonSecondary from './ButtonSecondary'
 import ButtonSidebar from './ButtonSidebar'
-import ButtonPrimary from './ButtonPrimary'
+import ButtonPrimary from './ButtonRegular'
 
 const ButtonControl = ({ control, ...rest }) => {
     switch(control){
         case 'sidebar':
             return <ButtonSidebar {...rest}/>
         case 'general':
-            return <ButtonGeneral {...rest}/>
+            return <ButtonSecondary {...rest}/>
         case 'primary':
             return <ButtonPrimary {...rest}/>
         default:
