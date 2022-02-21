1de61d6623e547a2e3ae5701fd9b5059840ed8ff
Mohammad Denny <mohammaddenny94@gmail.com>
2022-02-22 06:03:30 +0700

    create comp

---
+++ client/src/components/table/TablePagination.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import ButtonControl from '../button/ButtonControl'
+
+const TablePagination = ({ 
+    gotoPage, 
+    previousPage, 
+    nextPage, 
+    canPreviousPage, 
+    canNextPage, 
+    pageIndex, 
+    pageCount, 
+    pageOptions, 
+    setPageSize, 
+    pageSize 
+}) => {
+    return (
+        <div className='space-x-1'>
+            <ButtonControl
+                control={'secondary'}
+                name={'first'}
+                label={'<<'}
+                onClick={() => gotoPage(0)}
+                disabled={!canPreviousPage}
+            />
+            <ButtonControl
+                control={'secondary'}
+                name={'previous'}
+                label={'Previous'}
+                onClick={() => previousPage()}
+                disabled={!canPreviousPage}
+            />
+            <ButtonControl
+                control={'secondary'}
+                name={'Next'}
+                label={'Next'}
+                onClick={() => nextPage()}
+                disabled={!canNextPage}
+            />
+            <ButtonControl
+                control={'secondary'}
+                name={'last'}
+                label={'>>'}
+                onClick={() => gotoPage(pageCount - 1)}
+                disabled={!canNextPage}
+            />
+            <span>
+                Page{' '}
+                <strong>
+                    {pageIndex + 1} of {pageOptions.length}
+                </strong>{' '}
+            </span>
+            <span>
+                | Go to page:{' '}
+                <input
+                    type='number'
+                    defaultValue={pageIndex + 1}
+                    onChange={e => {
+                        const pageNumber = e.target.value ? Number(e.target.value) - 1 : 0
+                        gotoPage(pageNumber)
+                    }}
+                    style={{ width: '50px' }}
+                />
+            </span>{' '}
+            <select
+                value={pageSize}
+                onChange={e => setPageSize(Number(e.target.value))}>
+                    {[10, 25, 50].map(pageSize => (
+                    <option key={pageSize} value={pageSize}>
+                        Show {pageSize}
+                    </option>
+                ))}
+            </select>
+        </div>
+    )
+}
+
+export default TablePagination
