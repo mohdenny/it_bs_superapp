7f6011a5db0e0defa0d92a9cf6b79517df5e5c1a
Mohammad Denny <mohammaddenny94@gmail.com>
2022-02-22 06:04:13 +0700

    Update TicketBar.js
    change general to secondary

--- client/src/components/ticket/TicketBar.js
+++ client/src/components/ticket/TicketBar.js
@@ -1,23 +1,33 @@
 import React from 'react'
 import ButtonControl from '../button/ButtonControl'
 
-const valuesButtonGeneral = [
-    { name: '', control: 'general', label: 'All' },
-    { name: 'open', control: 'general', label: 'Open' },
-    { name: 'in progress', control: 'general', label: 'In Progress' },
-    { name: 'pending', control: 'general', label: 'Pending' },
-    { name: 'on hold', control: 'general', label: 'On Hold' },
-    { name: 'escalated', control: 'general', label: 'Escalated' },
-    { name: 'solved', control: 'general', label: 'Solved' },
-    { name: 'closed', control: 'general', label: 'Closed' }
+const valuesButtonSecondary = [
+    { name: '', control: 'secondary', label: 'All' },
+    { name: 'open', control: 'secondary', label: 'Open' },
+    { name: 'in progress', control: 'secondary', label: 'In Progress' },
+    { name: 'pending', control: 'secondary', label: 'Pending' },
+    { name: 'on hold', control: 'secondary', label: 'On Hold' },
+    { name: 'escalated', control: 'secondary', label: 'Escalated' },
+    { name: 'solved', control: 'secondary', label: 'Solved' },
+    { name: 'closed', control: 'secondary', label: 'Closed' }
 ]
 
-const TicketBar = ({ handleCallModal, ...rest }) => {
+const TicketBar = ({ 
+    handleCallModal,
+    // countTicketByStatus, 
+    ticketToCount, 
+    totalCount, 
+    setTerm, 
+    ...rest 
+}) => {
+
+    console.log(totalCount)
+
     return (
         <div className='flex flex-col items-center justify-center py-6 space-y-4 bg-white h-auto w-full '>
             <div className='flex items-center justify-center h-auto w-full'>
                 <ButtonControl
-                    control='primary'
+                    control='regular'
                     label='Create New'
                     icon='search'
                     color='blue'
@@ -27,15 +37,23 @@ const TicketBar = ({ handleCallModal, ...rest }) => {
             </div>
             <div className='flex flex-row items-center justify-center h-auto space-x-4 w-full'>
                 {
-                    valuesButtonGeneral.map((item, index) => {
+                    valuesButtonSecondary.map((item, index) => {
                         return (
                             <ButtonControl
                                 key={index}
                                 control={item.control}
                                 name={item.name}
                                 label={item.label}
+                                datas={ticketToCount}
                                 {...rest}
-                            />
+                            >
+                                {/* {
+                                    ticketToCount && countTicketByStatus(ticketToCount, item.name) ?
+                                        <sup> { countTicketByStatus(ticketToCount, item.name)}</sup> 
+                                        : 
+                                        null
+                                } */}
+                            </ButtonControl>
                         )
                     })
                 }
