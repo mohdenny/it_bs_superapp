dc707ff8def976c5a41b27a8ad3909c18e96283e
Mohammad Denny <mohammaddenny94@gmail.com>
2022-05-02 23:23:49 +0700

    import action

--- client/src/actions/report.js
+++ client/src/actions/report.js
@@ -1,5 +1,5 @@
-import { dataReportProgram } from '../utils/staticData' 
-import { GET_PROGRAMS, REPORT_ERROR, CLEAR_REPORT, GET_PROGRAM } from './types' 
+import { dataReportProgram, dataReportLive } from '../utils/staticData' 
+import { GET_PROGRAMS, GET_PROGRAM, GET_LIVES, GET_LIVE, REPORT_ERROR, CLEAR_REPORT } from './types' 
 
 // get all programs report
 export const getPrograms = () => async dispatch => {
@@ -27,6 +27,45 @@ export const getProgramById = id => async dispatch => {
 
         dispatch({
             type: GET_PROGRAM,
+            // payload: res.data
+            payload: res.filter(item => item.id === id)
+        })
+    } catch (err) {
+        dispatch({
+            type: REPORT_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        })
+    }
+}
+
+// get all lives report
+export const getLives = () => async dispatch => {
+    dispatch({ type: CLEAR_REPORT })
+
+    try {
+        const res = await dataReportLive
+
+        console.log(dataReportLive)
+
+        dispatch({
+            type: GET_LIVES,
+            payload: res
+        })
+    } catch (err) {
+        dispatch({
+            type: REPORT_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        })
+    }
+}
+
+//  get live by id
+export const getLiveById = id => async dispatch => {
+    try {
+        const res = await dataReportLive
+
+        dispatch({
+            type: GET_LIVE,
             // payload: res.data
             payload: res.filter(item => item.id === id)
         })
