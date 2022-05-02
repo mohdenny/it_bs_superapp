4863c0747100367fdab97771250e42ff6a8aef8e
Mohammad Denny <mohammaddenny94@gmail.com>
2022-05-02 23:23:19 +0700

    create component live column table

---
+++ client/src/components/report/LiveColumnTable.js
@@ -0,0 +1,85 @@
+import { format } from 'date-fns'
+
+export const LIVECOLUMNSTABLE = [
+    {
+        Header: 'Program',
+        Footer: 'Program',
+        accessor: 'program',
+        sticky: 'left',
+        Cell: ({ row, value, onClick }) => {
+            return (
+                <div className='ml-4'>
+                    <button 
+                        className="text-sm text-left text-gray-900 font-bold uppercase hover:text-gray-500 break-words w-full" 
+                        onClick={() => onClick('modal-detail', row.original.id)}
+                    >
+                        {value}
+                    </button>
+                </div>
+            )
+        }
+    },
+    {
+        Header: 'Pukul',
+        Footer: 'Pukul',
+        accessor: 'pukul'
+    },
+    {
+        Header: 'Unit',
+        Footer: 'Unit',
+        accessor: 'unit'
+    },
+    {
+        Header: 'Server',
+        Footer: 'Server',
+        accessor: 'server'
+    },
+    {
+        Header: 'Modem Up',
+        Footer: 'Modem Up',
+        accessor: 'modem up'
+    },
+    {
+        Header: 'Lokasi',
+        Footer: 'Lokasi',
+        accessor: 'lokasi',
+    },
+    {
+        Header: 'Description',
+        Footer: 'Description',
+        accessor: 'description',
+    },
+    {
+        Header: 'Date',
+        Footer: 'Date',
+        accessor: 'date',
+        Cell: ({ value }) => {
+            return format(new Date(value), 'dd/MM/yyyy HH:mm')
+        }
+    },
+    {
+        Header: 'Action',
+        Footer: 'Action',
+        accessor: 'action',
+        Cell: ({ row, onClick, ButtonControl }) => {
+            return (
+                <>
+                    <ButtonControl
+                        control='primary'
+                        icon='pencil'
+                        color='gray'
+                        textColor='white'
+                        onClick={() => onClick('modal-edit-form', row.original.id)}
+                    />
+                    <ButtonControl
+                        control='primary'
+                        icon='trash'
+                        color='red'
+                        textColor='white'
+                        onClick={null}
+                    />
+                </>
+            )
+        }
+    },
+]
