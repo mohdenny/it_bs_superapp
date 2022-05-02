d5bb57e4cd5be38af02c625ccb2bd2363ea4680b
Mohammad Denny <mohammaddenny94@gmail.com>
2022-05-02 23:23:37 +0700

    create new component line position

--- client/src/components/menu/FirstLine.js
+++ client/src/components/menu/FirstLine.js
@@ -2,8 +2,10 @@ import React from "react"
 
 const FirstLine = ({ children }) => {
     return (
-        <div className='flex items-center justify-center h-auto w-full'>
-            {children}
+        <div className='flex flex-col items-center justify-center space-y-4 bg-white h-auto w-full '>
+            <div className='flex flex-row items-center justify-center h-auto space-x-4 w-full'>
+                {children}
+            </div>
         </div>
     )
 }
